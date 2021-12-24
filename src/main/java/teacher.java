56bf83d9deb97199ea41669677f8190b316d9032
erku <2965573243@qq.com>
2021-12-24 14:23:14 +0800

    idea seconde commit test

--- src/main/java/teacher.java
+++ src/main/java/teacher.java
@@ -1,5 +1,7 @@
 public class teacher {
     public static void main(String[] args) {
         System.out.println("hello teachher");
+        System.out.println("hello teachher");
+        System.out.println("hello teachher");
     }
 }
