384774b509eab53a4a6f629d838f40adf6c88e0c
erku <2965573243@qq.com>
2021-12-25 08:10:05 +0800

    idea master commit third test

--- src/main/java/teacher.java
+++ src/main/java/teacher.java
@@ -2,5 +2,6 @@ public class teacher {
     public static void main(String[] args) {
         System.out.println("hello teachher");
         System.out.println("分支分支分支");
+        System.out.println("master 修改");
     }
 }
