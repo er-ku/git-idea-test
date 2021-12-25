011560135fcf530cca055a84a4c394b6fb7dfd58
er-ku <95110629+er-ku@users.noreply.github.com>
2021-12-25 08:34:53 +0800

    idea pull test

--- src/main/java/teacher.java
+++ src/main/java/teacher.java
@@ -5,5 +5,8 @@ public class teacher {
         System.out.println("master 修改");
         System.out.println("分支第二次提交");
         System.out.println("push 更新远程仓库");
+         System.out.println("pull 拉取远程仓库");
+        
+        
     }
 }
